dcd8776bcc03cc768f0d78c6199e625b596aed44
kdongjin <rlaeogus0911@nate.com>
2026-07-24 12:18:31 +0900

    [리액트 자바크스립트] 객체 순회처리방식 키 배열, 값 배열 출력하는 기능 , foreach 테스트

---
+++ section01/chapter13.js
@@ -0,0 +1,23 @@
+//객체를 순회해서 출력하기
+const person = { 
+name: "김동진", 
+age: 25, 
+tall: 179 
+};  
+
+console.log(person)
+//객체에 키값을 배열로 가져와서 출력
+let keyArray = Object.keys(person)
+let valueArray = Object.values(person)
+console.log(keyArray)
+
+keyArray.forEach((e)=> console.log(`${e} : ${person[e]}`))
+valueArray.forEach((e)=>console.log(`${e} ${typeof e}`))
+
+for (const e of keyArray) {
+  console.log(`${e} : ${person[e]}`)
+}
+console.log("-----------------")
+for (const e of valueArray) {
+  console.log(`${e} ${typeof e}`)
+}
